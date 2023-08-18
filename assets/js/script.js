348fd6698aa071633c79743f6fc0032f3febb54f
alexandrazykova <alexandrazykova1@gmail.com>
2023-08-18 16:21:08 -0400

    fixed syntax bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,40 @@
-// Wraps all code that interacts with the DOM so it isn't run 
-// until the browser has finished rendering whole HTML
+// Wraps all code to finish rendering HTML before running
 $(document).ready(function(){
-
-
-
 
 // Display the current date in the header
 var now = dayjs();
 $("#currentDay").text(now.format('dddd, MMMM D, YYYY'));
 
+// selecting all saveBtn(s)
+var saveBtn = $(".saveBtn"); 
 
-var saveBtn = $(".saveBtn"); // selecting all saveBtn(s)
+// Loop through selected time block elements
+$(".time-block").each(function () {
+    var timeBlockEl = $(this).attr("id");
+    var savedDescription = localStorage.getItem(timeBlockEl);
+    
+    // Displays saved description value in the textarea
+    if (savedDescription !== null) {
+      $(this).find(".description").val(savedDescription);
+    }
+});
+
 // Add a click event listener to save buttons
 saveBtn.on("click", function () {
     // for the button being clicked selecting immed parent with time-block class and value id
     var timeEl = $(this).parent(".time-block").attr("id");
-
     console.log("Saved", timeEl);
+
     // Selecting sibling elem with class description, retrieves entered users content
     var description = $(this).siblings(".description").val();
-
     console.log("Description:", description);
+
+    localStorage.setItem(timeEl, description);
+
     });
+
+   
+
+
+
 });
-// How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
